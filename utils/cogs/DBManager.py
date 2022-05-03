225bc0b9a5ae6ec9b9b0bc4ff880cb58f6ef0caf
jellayy <austinlynnhuffman@gmail.com>
2022-05-03 16:25:45 -0700

    changed formatting for db get

--- utils/cogs/DBManager.py
+++ utils/cogs/DBManager.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 import utils.db.db_driver as db_driver
 
@@ -9,8 +10,12 @@ class DBManager(commands.Cog):
 
     @commands.command()
     async def get_db(self, ctx):
-        for row in db_driver.get_users(self.client.db_cur):
-            await ctx.send(row)
+        with open('utils/db/db_print.txt', 'w') as f:
+            f.write("Discord User | Plex Email | Plex Server\n")
+            for row in db_driver.get_users(self.client.db_cur):
+                f.write(f"{row[0]} | {row[1]} | {row[2]}\n")
+        with open('utils/db/db_print.txt', 'rb') as file:
+            await ctx.send(file=discord.File(file, 'utils/db/db_print.txt'))
 
 
 def setup(client):
